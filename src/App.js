21fc1ff716afb2d5fe93bb843591f4df1ba2850c
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2023-07-04 15:24:29 +0530

    Install Mulish font & update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import Navbar from "./components/navbar/Navbar";
 import LandingComponent from "./components/landingInterface/LandingComponent";
+import PaymentSuiteBox from "./components/paymentSuiteHeader/PaymentSuiteBox";
 import PaymentSuite from "./components/paymentSuite/PaymentSuite";
+import Footer from "./components/footer/Footer";
+import FooterComponent from "./components/footer/FooterComponent"
+import PaymentSuiteHeader from "./components/paymentSuiteHeader/paymentSuiteHeader";
+import RazorpayCustomers from "./components/razorpayCustomers/RazorpayCustomers";
+import Testimonials from "./components/testimonials/Testimonials";
+import RazopayX from "./components/razorpayX/RazorpayX";
+import Features from "./components/features/Features";
+import NewFeatures from "./components/newFeatures/NewFeatures";
+import NavigationBar from "./components/navbar/NavigationBar";
+import LandingInterface from "./components/landingInterface/landingInterface";
 
 function App() {
   return (
-      <>
-          <Navbar/>
-          <LandingComponent/>
+      <div className="overflow-x-hidden relative w-full">
+          <NavigationBar/>
+          <LandingInterface/>
           <PaymentSuite/>
-      </>
+          <RazopayX/>
+          {/*<NewFeatures/>*/}
+          {/*<Features/>*/}
+          <RazorpayCustomers/>
+          <Testimonials/>
+          <FooterComponent/>
+          <Footer/>
+      </div>
   );
 }
 
