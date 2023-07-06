a53062732abf0e5aca84bcf2dfcc133efe1aa3ba
Aashish Kumar <JMPHEDAASHISH@GMAIL.COM>
2023-07-06 16:54:19 +0530

    Update UI Components in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import Navbar from "./components/navbar/Navbar";
-import LandingComponent from "./components/landingInterface/LandingComponent";
-import PaymentSuiteBox from "./components/paymentSuiteHeader/PaymentSuiteBox";
 import PaymentSuite from "./components/paymentSuite/PaymentSuite";
 import Footer from "./components/footer/Footer";
 import FooterComponent from "./components/footer/FooterComponent"
-import PaymentSuiteHeader from "./components/paymentSuiteHeader/paymentSuiteHeader";
 import RazorpayCustomers from "./components/razorpayCustomers/RazorpayCustomers";
 import Testimonials from "./components/testimonials/Testimonials";
 import RazopayX from "./components/razorpayX/RazorpayX";
@@ -14,20 +10,20 @@ import NavigationBar from "./components/navbar/NavigationBar";
 import LandingInterface from "./components/landingInterface/landingInterface";
 
 function App() {
-  return (
-      <div className="overflow-x-hidden relative w-full">
-          <NavigationBar/>
-          <LandingInterface/>
-          <PaymentSuite/>
-          <RazopayX/>
-          {/*<NewFeatures/>*/}
-          {/*<Features/>*/}
-          <RazorpayCustomers/>
-          <Testimonials/>
-          <FooterComponent/>
-          <Footer/>
-      </div>
-  );
+    return (
+        <div className="overflow-x-hidden relative w-full">
+            <NavigationBar/>
+            <LandingInterface/>
+            <PaymentSuite/>
+            <RazopayX/>
+            <NewFeatures/>
+            <Features/>
+            <RazorpayCustomers/>
+            <Testimonials/>
+            <FooterComponent/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
